1d331e083b48836a14676a3b0444cb1dd35d3a63
Foeland <espen_game@live.no>
2015-03-12 22:56:39 +0100

    Done with all three tutorials, but got some wrongs i dont know how to fix

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,22 +1,24 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+	#...
+	model = Choice
+	extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets =[
-		(none,       {'fields': ['question_text']}),
-		('Date information', {'fields': ['pub_date']})
-
-	]
-
-
-
-
-
-
-
-
-
-
+		(None,       {'fields': 
+['question_text']}),
+		('Date information', {'fields': ['pub_date'],
+'classes': ['collapse']}),
+  	]
+  	list_display = ('question_text', 'pub_date', 'was_published_recently')
+  	inlines = [ChoiceInline]
+  	list_filter = ['pub_date']
+  	search_fields = ['question_text']
+ 
 
 
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -16,8 +16,8 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
-	
-# ...
+	#...
+
 	def __str__(self): #_unicode_ on Python 2
 		return self.choice_text
 
